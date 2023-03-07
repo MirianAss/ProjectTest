6a6934ea2ce6aa8817704b2c2cde41942ed904f5
Mirian Santana <miriansantana93@gmail.com>
2023-03-07 14:00:52 -0300

    added Luxury Brands class, with launcher

---
+++ ProjectWifhGithub/ProjectWifhGithub/LuxuryBrands.swift
@@ -0,0 +1,24 @@
+//
+//  LuxuryBrands.swift
+//  ProjectWifhGithub
+//
+//  Created by Mirian Santana on 07/03/23.
+//
+
+import UIKit
+
+class LuxuryBrands: NSObject {
+    var imageOne: String
+    var imageTwo: String
+    var title: String
+    var text: String
+    
+    init(imageOne: String, imageTwo: String, title: String, text: String) {
+        self.imageOne = imageOne
+        self.imageTwo = imageTwo
+        self.title = title
+        self.text = text
+    }
+    
+    
+}
