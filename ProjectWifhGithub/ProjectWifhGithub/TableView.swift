e6aa3d92f3852d3c2d9c474b65641efd2a461891
Mirian Santana <miriansantana93@gmail.com>
2023-03-07 13:23:46 -0300

    added table view class with its connection in storyboard

---
+++ ProjectWifhGithub/ProjectWifhGithub/TableView.swift
@@ -0,0 +1,31 @@
+//
+//  TableView.swift
+//  ProjectWifhGithub
+//
+//  Created by Mirian Santana on 07/03/23.
+//
+
+import UIKit
+
+class TableView: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
